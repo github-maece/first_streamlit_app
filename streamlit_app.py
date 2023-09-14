70903284b67b81d36b2ed3d175cb81679b3e0294
github-maece <144969647+github-maece@users.noreply.github.com>
2023-09-14 10:06:15 +0800

    Update streamlit_app.py
    correct spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 and Blueberryb Oatmeal')
+streamlit.text('🥗 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
